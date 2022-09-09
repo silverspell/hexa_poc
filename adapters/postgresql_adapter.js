5cb73220a261fee0ceb97e5a196be9088b132b48
Cem GULER <cem@marti.tech>
2022-09-09 15:24:14 +0300

    postgresql adapter stub is added for example

---
+++ adapters/postgresql_adapter.js
@@ -0,0 +1,18 @@
+function postgresqlAdapter() {
+
+    let _conn;
+
+    const connect = async () => {
+        // some code to connect db
+        _conn = {};
+    }
+
+    const findAll = async () => {
+        return _conn.query("select * from users");
+    }
+
+    const addUser = async (user) => {
+        const {username, password} = user;
+        _conn.query("insert into users (username, password) values ($1, $2)", [username, password]);
+    }
+}
