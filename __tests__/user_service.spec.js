4d7d7c3850d9be6f773ae0ee74cc5207655b2ef9
Cem GULER <cem@marti.tech>
2022-09-11 10:53:57 +0300

    renamings...

---
+++ __tests__/user_service.spec.js
@@ -0,0 +1,43 @@
+const UsersService = require("../domain/services/user_service");
+
+
+const mockRepo = {
+    users: [
+        {username: "cemg", password: "1234"},
+        {username: "mustafau", password: "1234"},
+    ],
+    findAll: async () => {
+        return mockRepo.users;
+    },
+
+    addUser: async (user) => {
+        return mockRepo.users.push(user);
+    }
+
+};
+
+describe("User Test Suite", () => {
+    test("it should return 2 values", async () => {
+        const userService = UsersService(mockRepo);
+        const userList = await userService.findAll();
+        expect(userList.length).toEqual(2);
+    });
+
+    test("it should add one user to the repo", async() => {
+        const user = {username: "another user", password: "12345"};
+        const userService = UsersService(mockRepo);
+        await userService.addUser(user);
+
+        const users = await userService.findAll();
+        const addedUser = users.filter(u => u.username == user.username && u.password == user.password);
+        expect(addedUser.length).toEqual(1);
+    });
+
+    test("it should return invalid username while adding", async() => {
+        const user = {password: "1111"};
+        const userService = UsersService(mockRepo);
+        const ret = await userService.addUser(user);
+
+        expect(ret).toEqual("invalid username");
+    });
+});
